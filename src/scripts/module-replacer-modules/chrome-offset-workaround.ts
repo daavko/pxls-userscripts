bcfe6405cc401d50a8295d50ae89b9136c7c4147
David Koníř <david.konir@gmail.com>
2025-05-19 06:22:00 +0200

    tmp

---
+++ src/scripts/module-replacer-modules/chrome-offset-workaround.ts
@@ -0,0 +1,23 @@
+import type { PxlsChromeOffsetWorkaroundModule } from '../../pxls/pxls-modules';
+import type { ModuleReplacementFunction } from './types';
+
+export const chromeOffsetWorkaroundModuleFn: ModuleReplacementFunction<'chromeOffsetWorkaround'> = (
+    requireFn,
+    moduleExport,
+) => {
+    'use strict';
+    const settings = requireFn('./settings');
+
+    const settingElement = $('#chrome-canvas-offset-setting');
+
+    const init = (): void => {
+        settings.fix.chrome.offset.enable.controls.remove(settingElement);
+        settingElement.parent().remove();
+    };
+    moduleExport.exports.chromeOffsetWorkaround = {
+        init,
+        update: (): void => {
+            /* intentionally empty */
+        },
+    } satisfies PxlsChromeOffsetWorkaroundModule;
+};
