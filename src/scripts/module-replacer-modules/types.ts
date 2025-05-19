b7e4e8c1d427b152c98975d4926acd88d312b9c8
David Koníř <david.konir@gmail.com>
2025-05-19 15:19:12 +0200

    progress

--- src/scripts/module-replacer-modules/types.ts
+++ src/scripts/module-replacer-modules/types.ts
@@ -3,7 +3,7 @@ import type { PxlsModules, PxlsModulesImportMap } from '../../pxls/pxls-modules'
 export type ModuleImportFunction = <T extends keyof PxlsModulesImportMap>(moduleName: T) => PxlsModulesImportMap[T];
 
 export interface ModuleExport<TModuleName extends keyof PxlsModules> {
-    exports: Record<TModuleName, PxlsModulesImportMap[`./${TModuleName}`]>;
+    exports: PxlsModulesImportMap[`./${TModuleName}`];
 }
 
 export type ModuleReplacementFunction<TModuleName extends keyof PxlsModules> = (
