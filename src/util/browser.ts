46c0c9c60b376a54e6bc6bbd16fa56c887b91b12
David Koníř <david.konir@gmail.com>
2025-10-24 12:26:44 +0200

    fix switching template url would throw an error in Firefox

--- src/util/browser.ts
+++ src/util/browser.ts
@@ -5,3 +5,19 @@ export async function waitForAnimationFrame(): Promise<void> {
     });
     return promise;
 }
+
+export function queueMacrotask(task: () => void): void {
+    const channel = new MessageChannel();
+    channel.port1.onmessage = (): void => {
+        task();
+    };
+    channel.port2.postMessage(undefined);
+}
+
+export async function waitForMacrotask(): Promise<void> {
+    const { promise, resolve } = Promise.withResolvers<void>();
+    queueMacrotask(() => {
+        resolve();
+    });
+    return promise;
+}
